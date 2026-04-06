f5d69e2cc1132e0d273f5aa95542abca737f5313
axelsilva17 <axelsilriq95@gmail.com>
2026-04-06 20:47:59 -0300

    fix(auth): unify auth interfaces; reintroduce IAutenticacionServicio compat to satisfy existing references

--- GestionComercial.Aplicacion/Interfaces/Autenticacion/IAutenticacionServicio.cs
+++ GestionComercial.Aplicacion/Interfaces/Autenticacion/IAutenticacionServicio.cs
@@ -1,6 +1,5 @@
 namespace GestionComercial.Aplicacion.Interfaces.Autenticacion
 {
-    // Nueva interfaz de compatibilidad: alias a IAuthService para evitar roturas
     public interface IAutenticacionServicio
     {
         bool IsCurrentUserAdmin();
