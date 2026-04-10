d31ac7c85b6cd9e3a2a043349897ccd1a1127697
axelsilva17 <axelsilriq95@gmail.com>
2026-04-10 14:22:35 -0300

    fix: enter key triggers product search in nueva compra

--- GestionComercial.UI/Views/Compras/CompraView.xaml.cs
+++ GestionComercial.UI/Views/Compras/CompraView.xaml.cs
@@ -1,20 +1,25 @@
 using System.Windows.Controls;
+using System.Windows.Input;
 using GestionComercial.UI.ViewModels.Compras;
 
 namespace GestionComercial.UI.Views.Compras
 {
     public partial class CompraView : UserControl
     {
-        public CompraView()
-        {
-            InitializeComponent();
-        }
+        private CompraViewModel? VM => DataContext as CompraViewModel;
+
+        public CompraView() => InitializeComponent();
 
         private void BuscarProducto_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (DataContext is CompraViewModel vm)
+            VM?.BuscarProducto();
+        }
+
+        private void BusquedaProducto_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
             {
-                vm.BuscarProducto();
+                VM?.BuscarProducto();
             }
         }
     }
